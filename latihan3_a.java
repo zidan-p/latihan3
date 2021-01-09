4f5bd49145d3783dad4724e6690bfa76b8839223
Zidan Putra Rahman <68506833+zidan-p@users.noreply.github.com>
2021-01-09 08:54:14 +0700

    Add files via upload

---
+++ latihan3_a.java
@@ -0,0 +1,18 @@
+
+package latihanPBO;
+
+
+public class latihan3_a {
+
+
+    public static void main(String[] args) {
+        int bil_a, bil_b, hasil;    //mendeklarasian varibel
+        bil_a = 454;    // memberi nilai pada vaariabel
+        bil_b = 546;    //memberi nilai pada varobel
+        hasil = bil_a + bil_b;  //melakukan operi hitng 
+        System.out.println("Hasil penjumlahan kedua bilangan adalah :"); // menmapilkan kalimat
+        System.out.println(hasil);  //menapilkan variabel hasi yang merupakan hasil dari operasi hitung
+        
+    }
+    
+}
